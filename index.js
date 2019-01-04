12372face8eb67c4b0650831aac81cbf0eda4869
Josephine Yao <yao.josephine@yahoo.com>
2019-01-04 06:20:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ function shout(string) {
   return string.toUpperCase();
 }
 
+function whisper(string) {
+  return string.toLowerCase();
+}
 
 /*describe('shout(string)', function() {
   it('receives one argument and returns it in all caps', function() {
