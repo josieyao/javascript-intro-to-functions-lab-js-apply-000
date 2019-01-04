b0e17342a80084a1543272c32ac44d4502afd86a
Josephine Yao <yao.josephine@yahoo.com>
2019-01-04 06:38:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ function logWhisper(string) {
 sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return "I can\'t hear you!";
-{ else if (string === uppercase) {
+{ else if (string.toUpperCase() === string) {
     return "YES INDEED!";
 } else {
     return "I love you, too."
